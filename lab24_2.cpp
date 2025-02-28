5dd79bad09b660e5812ac0bc714d72c2532c905a
goddamnshit <pongthitheethawat@gmail.com>
2025-02-28 15:22:49 +0700

    Update lab24_2.cpp

--- lab24_2.cpp
+++ lab24_2.cpp
@@ -4,28 +4,61 @@
 
 using namespace std;
 
-void printList(list<string> s){
-	list<string>::iterator i;
-	for( i = s.begin(); i != s.end(); i++){
-		cout << *i << " ";
-	}		
-	cout << "\n";	
+void printList(list<string> s) {
+    list<string>::iterator i;
+    for (i = s.begin(); i != s.end(); i++) {
+        cout << *i << " ";
+    }
+    cout << "\n";
 }
 
-int main(){
-	
-	list<string> line_up;
-	list<string>::iterator loc;
-	
-	line_up.push_back("Alice");
-	line_up.push_back("Bob");
-	
-	loc = find(line_up.begin(),line_up.end(),"Bob");
-	line_up.insert(loc,"Oscar");
-	
-	//Write your code here
-	
-	printList(line_up);
-		
-	return 0;
+int main() {
+    list<string> line_up;
+    list<string>::iterator loc;
+
+    // Alice stands in the queue first
+    line_up.push_back("Alice");
+
+    // Bob joins the queue
+    line_up.push_back("Bob");
+
+    // Oscar cuts in front of Bob
+    loc = find(line_up.begin(), line_up.end(), "Bob");
+    line_up.insert(loc, "Oscar");
+
+    // Luffy, Sanji, Nami join at the back
+    line_up.push_back("Luffy");
+    line_up.push_back("Sanji");
+    line_up.push_back("Nami");
+
+    // First two people (Alice and Oscar) go into the restroom
+    line_up.pop_front();  // Alice
+    line_up.pop_front();  // Oscar
+
+    // Naruto cuts after Luffy
+    loc = find(line_up.begin(), line_up.end(), "Luffy");
+    line_up.insert(++loc, "Naruto");
+
+    // Prayath cuts at the front
+    line_up.push_front("Prayath");
+
+    // Tony joins after Prayath
+    loc = find(line_up.begin(), line_up.end(), "Prayath");
+    line_up.insert(++loc, "Tony");
+
+    // Bob leaves the queue
+    loc = find(line_up.begin(), line_up.end(), "Bob");
+    if (loc != line_up.end()) {
+        line_up.erase(loc);  // Remove Bob
+    }
+
+    // First three people (Oscar, Luffy, Sanji) go into the restroom
+    line_up.pop_front();  // Oscar
+    line_up.pop_front();  // Luffy
+    line_up.pop_front();  // Sanji
+
+    // Print the final list
+    printList(line_up);
+
+    return 0;
 }
